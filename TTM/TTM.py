5f3d6b0acaa9cc2368f0554949a34f6c91e573e1
David Russell <russeldj@clarkson.edu>
2019-03-12 16:48:11 -0400

    First step of including keypoints in tracking process

--- TTM/TTM.py
+++ TTM/TTM.py
@@ -12,6 +12,7 @@ Most operations that I want to run will be functions in here which can just be c
 
 """
 VIDEO_FILE = "/home/drussel1/data/EIMP/videos/Injection_Preparation.mp4"
+KEYPOINTS_FILE = "./data/TTM-data/processed/EIMP/Mask-Guided-Keypoints/Injection_Preparation"
 START_FRAME = 300
 
 def computeOpenPoseKeypoints():
@@ -26,16 +27,20 @@ def FullTracking():
     from libs.DaSiamRPN.code.SiamRPN_tracker import SiamRPN_tracker
     print(SiamRPN_tracker)
 
-def loadKeypoints(foldername="./data/TTM-data/processed/EIMP/Mask-Guided-Keypoints/Blood_Glucose-6Q-UJJmTMQA/"):
+def loadKeypoints(foldername=KEYPOINTS_FILE):
     keypoint_capture = KeypointCapture.Read2DJsonPath(foldername, 0, 0)
-    for i in range(keypoint_capture.num_frames):
-        print(keypoint_capture.GetFrameKeypointsAsOneDict(i), i)
-    print(keypoint_capture.GetFrameKeypointsAsOneDict(0))
+    return keypoint_capture
+    #for i in range(keypoint_capture.num_frames):
+    #    print(keypoint_capture.GetFrameKeypointsAsOneDict(i), i)
+    #print(keypoint_capture.GetFrameKeypointsAsOneDict(0))
 
 def testDaSiamTracking(video_fname=VIDEO_FILE):
+    # import the tracker
     from libs.DaSiamRPN.code.SiamRPN_tracker import SiamRPN_tracker
     LOST_THRESH = 0.8
     OUTPUT_FILENAME = "video.avi"
+    SELECT_REGION = False # choose your own initial region
+    SET_SEARCH = True # specify where to look
     FPS = 30
     WIDTH = 1280
     HEIGHT = 720
@@ -50,32 +55,59 @@ def testDaSiamTracking(video_fname=VIDEO_FILE):
     video_reader.set(1, START_FRAME)
     ok, frame = video_reader.read()
     
-    ltwh = list(cv2.selectROI(frame))
+    # choose the initial region
+    if SELECT_REGION:
+        ltwh = list(cv2.selectROI(frame))
+    else:
+        ltwh = [813, 459, 43, 109]
+    print(ltwh)
     hand_box = [float(i) for i in ltwh]
     track_1_diff_xy = [0.0, 0.0]
     cv2.destroyAllWindows()
+    # create a wrapper around the tracker
     tracker = SiamRPN_tracker(frame, ltwh) 
+    tracker.setSearchRegion(ltwh)
 
-    while ok:
-        cv2.rectangle(frame, (ltwh[0], ltwh[1]), (ltwh[0] + ltwh[2], ltwh[1] + ltwh[3]), (255,0,0) , 3)
-        cv2.putText(frame, "conf: {:03f}".format(score), (ltwh[0], ltwh[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
-        cv2.imshow('SiamRPN', frame)
-        video_writer.write(frame)
-        cv2.waitKey(1)
-        ok, frame = video_reader.read()
-        tic = cv2.getTickCount()
-        ltwh, score, crop_region = tracker.predict(frame)  # track
-        crop_region = [int(c) for c in crop_region]
-        cv2.rectangle(frame, (crop_region[0], crop_region[1]), (crop_region[2], crop_region[3]), (0,0,255) , 3)
-        cv2.putText(frame, "search window", (crop_region[0], crop_region[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
-        print(score)
-        toc += cv2.getTickCount()-tic
-        frame_num += 1
+    if SET_SEARCH:
+        keypoint_capture = loadKeypoints()
+        i = 0
+        while ok:
+            current_keypoints = keypoint_capture.GetFrameKeypointsAsOneDict(i)
+            print(current_keypoints)
+            cv2.rectangle(frame, (ltwh[0], ltwh[1]), (ltwh[0] + ltwh[2], ltwh[1] + ltwh[3]), (255,0,0) , 3)
+            cv2.putText(frame, "conf: {:03f}".format(score), (ltwh[0], ltwh[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
+            cv2.imshow('SiamRPN', frame)
+            video_writer.write(frame)
+            cv2.waitKey(1)
+            ok, frame = video_reader.read()
+            tic = cv2.getTickCount()
+            #tracker.setSearchRegion([100, 100, 50, 100]) # set search region
+            ltwh, score, crop_region = tracker.predict(frame)  # track
+            crop_region = [int(c) for c in crop_region]
+            cv2.rectangle(frame, (crop_region[0], crop_region[1]), (crop_region[2], crop_region[3]), (0,0,255) , 3)
+            i += 1
+
+    else:
+        while ok:
+            cv2.rectangle(frame, (ltwh[0], ltwh[1]), (ltwh[0] + ltwh[2], ltwh[1] + ltwh[3]), (255,0,0) , 3)
+            cv2.putText(frame, "conf: {:03f}".format(score), (ltwh[0], ltwh[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
+            cv2.imshow('SiamRPN', frame)
+            video_writer.write(frame)
+            cv2.waitKey(1)
+            ok, frame = video_reader.read()
+            tic = cv2.getTickCount()
+            ltwh, score, crop_region = tracker.predict(frame)  # track
+            crop_region = [int(c) for c in crop_region]
+            cv2.rectangle(frame, (crop_region[0], crop_region[1]), (crop_region[2], crop_region[3]), (0,0,255) , 3)
+            cv2.putText(frame, "search window", (crop_region[0], crop_region[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
+            print(score)
+            toc += cv2.getTickCount()-tic
+            frame_num += 1
 
 
 if __name__ == "__main__":
-    loadKeypoints()
-    #testDaSiamTracking()
+    #loadKeypoints()
+    testDaSiamTracking()
     #FullTracking()
     #computeOpenPoseKeypoints()
 
