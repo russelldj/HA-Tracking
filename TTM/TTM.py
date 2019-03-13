003237c264a9e40a59852e74696f1df3ac010b8d
David Russell <russeldj@clarkson.edu>
2019-03-13 17:07:37 -0400

    working demo with setting the search region based on the pointer finger

--- TTM/TTM.py
+++ TTM/TTM.py
@@ -4,7 +4,7 @@ import argparse
 import cv2
 import pdb
 
-from tools import tools, mask, KeypointCapture
+from tools import tools, mask, KeypointCapture, KeypointVisualization
 
 """
 THis is going to do everything
@@ -12,7 +12,7 @@ Most operations that I want to run will be functions in here which can just be c
 
 """
 VIDEO_FILE = "/home/drussel1/data/EIMP/videos/Injection_Preparation.mp4"
-KEYPOINTS_FILE = "./data/TTM-data/processed/EIMP/Mask-Guided-Keypoints/Injection_Preparation"
+KEYPOINTS_FILE = "./data/TTM-data/processed/EIMP/Mask-Guided-Keypoints-Zero-Padded/Injection_Preparation"
 START_FRAME = 300
 
 def computeOpenPoseKeypoints():
@@ -38,7 +38,8 @@ def testDaSiamTracking(video_fname=VIDEO_FILE):
     # import the tracker
     from libs.DaSiamRPN.code.SiamRPN_tracker import SiamRPN_tracker
     LOST_THRESH = 0.8
-    FINGER_CONF = 0.7
+    FINGER_CONF = 0.2#0.2 # still needs to be further tuned
+    FINGER = "Right_Index4"
     OUTPUT_FILENAME = "video.avi"
     SELECT_REGION = False # choose your own initial region
     SET_SEARCH = True # specify where to look
@@ -49,6 +50,8 @@ def testDaSiamTracking(video_fname=VIDEO_FILE):
     score = 1
     toc = 0
     frame_num = START_FRAME
+    
+    # create the visualizer
     
     fourcc = cv2.VideoWriter_fourcc(*"MJPG")
     video_writer = cv2.VideoWriter(OUTPUT_FILENAME, fourcc, FPS, (WIDTH, HEIGHT))
@@ -70,20 +73,25 @@ def testDaSiamTracking(video_fname=VIDEO_FILE):
     tracker.setSearchRegion(ltwh)
 
     if SET_SEARCH:
-        keypoint_capture = loadKeypoints()
-        i = 0
+        keypoint_capture = loadKeypoints() # the issue here is that there is that the jsons are not zero padded
+        visualizer = KeypointVisualization.KeypointVisualization(keypoint_capture)
+        i = START_FRAME
         while ok:
             current_keypoints = keypoint_capture.GetFrameKeypointsAsOneDict(i)
-            if "Right_Index1" in current_keypoints and current_keypoints["Right_Index1"][2] != 0:
-                right_index = current_keypoints["Right_Index1"]
-                print("the confs are {}".format([x[2] for x in current_keypoints.values()]))
+            if FINGER in current_keypoints and current_keypoints[FINGER][2] != 0:
+                right_index = current_keypoints[FINGER]
+                #print("the confs are {}".format([x[2] for x in current_keypoints.values()]))
                 # this threshold needs to be tuned
-                if right_index[2] > FINGER_CONF:
+                if right_index[2] > FINGER_CONF and tracker.isLost():
                     print("right index {}".format(right_index))
                     tracker.setSearchLocation(right_index[0:2])
+                    cv2.circle(frame, tuple([int(x) for x in right_index[0:2]]), 5, (255,0,0) , 10)
 
             cv2.rectangle(frame, (ltwh[0], ltwh[1]), (ltwh[0] + ltwh[2], ltwh[1] + ltwh[3]), (255,0,0) , 3)
             cv2.putText(frame, "conf: {:03f}".format(score), (ltwh[0], ltwh[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
+
+            # add the keypoints
+            frame = visualizer.PlotSingleFrameFromIndOpenCV(frame, i)
             cv2.imshow('SiamRPN', frame)
             video_writer.write(frame)
             cv2.waitKey(1)
